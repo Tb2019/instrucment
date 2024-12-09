79f66fd120062e7dc8615bd5bdbd9ae8ada613ab
Tb2019 <982148620@qq.com>
2024-12-09 16:23:31 +0800

    完善中间件逻辑
    1、为请求增加代理，引入代理池，获取代理
    2、为普通请求和pdf下载请求设置不同的请求头
    3、起始设置默认代理和请求头、session。遇错即全部更换，重发请求
    4、增加了异常捕获，稳定程序运行

--- instrument/middlewares.py
+++ instrument/middlewares.py
@@ -11,6 +11,7 @@ import requests
 from scrapy.http import HtmlResponse
 from fake_useragent import UserAgent
 from instrument.Pdf_url_request import PdfUrlRequest
+from instrument.get_proxy import ProxyPool
 
 # useful for handling different item types with a single interface
 from itemadapter import is_item, ItemAdapter
@@ -122,7 +123,7 @@ class request_middleware:
         self.session = requests.Session()  # 创建一个请求会话
         # 获取pdf链接用到以下配置
         self.url = "https://www.instrument.com.cn/netshow/combo/paper/getAttachmentUrl"
-        self.headers = {
+        self.pdf_headers = {
                           'Cache-Control': 'no-cache',
                           'Connection': 'keep-alive',
                           'Cookie': 'sajssdk_2015_cross_new_user=1; 3i=2024120610080925; isnotice=2; isfirstin=false; mobileValid=85de113a163b97513476bd80075ed6e2; imtoken=957c715e107be62039d81a6caa09af1a; CheckValid=25277ff477881fe3d333edfb77b70dbb; imstep=1733450951; passwordValid=b7be5bfb0e9daf2c24663eddcdd96b59; useid=6825953; mobile=b47ebe9bea209181829fecc5b23a29ff; nickname=Ins%255f98d68598; username_guestbook=6825953; userType=0; privacyVersion=62; username=Ins_98d68598; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22Ins_98d68598%22%2C%22first_id%22%3A%2219399b9799116e5-0eaebd7d39cd4a8-4c657b58-1821369-19399b9799214c4%22%2C%22props%22%3A%7B%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTkzOTliOTc5OTExNmU1LTBlYWViZDdkMzljZDRhOC00YzY1N2I1OC0xODIxMzY5LTE5Mzk5Yjk3OTkyMTRjNCIsIiRpZGVudGl0eV9sb2dpbl9pZCI6Ikluc185OGQ2ODU5OCJ9%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%24identity_login_id%22%2C%22value%22%3A%22Ins_98d68598%22%7D%2C%22%24device_id%22%3A%2219399b9799116e5-0eaebd7d39cd4a8-4c657b58-1821369-19399b9799214c4%22%7D; sensorsKey=Ins_98d68598; HMF_CI=;',
@@ -140,6 +141,9 @@ class request_middleware:
                           'sec-ch-ua-platform': '"Windows"',
                           'x-requested-with': 'XMLHttpRequest'
                         }
+        self.headers = {'User-Agent': UserAgent().random}
+        self.pool = ProxyPool()
+        self.proxy = self.pool.load_proxy(from_clash=True)
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -155,21 +159,39 @@ class request_middleware:
 
         # 使用 requests 会话发送请求
         def send_request():
-            if isinstance(request, PdfUrlRequest):
-                payload = {'paperId': f'{request.meta["pdf_serial"]}',
-                           'pType': '1',
-                           'device': 'PC'}
-                files = [
+            try:
+                if isinstance(request, PdfUrlRequest):
+                    payload = {'paperId': f'{request.meta["pdf_serial"]}',
+                               'pType': '1',
+                               'device': 'PC'}
+                    files = [
 
-                ]
-                response = requests.request("POST", self.url, headers=self.headers, data=payload, files=files)
-            else:
-                # 发送同步请求
-                response = self.session.get(request.url, headers={'User-Agent': UserAgent().random})
+                    ]
+                    response = requests.request("POST", self.url, headers=self.pdf_headers, data=payload, files=files, proxies=self.proxy)
+                else:
+                    # 发送同步请求
+                    response = self.session.get(request.url, headers=self.headers, proxies=self.proxy)
+            except:
+                self.proxy = self.pool.load_proxy(from_clash=True)
+                self.pdf_headers['User-Agent'] = UserAgent().random
+                self.headers['User-Agent'] = UserAgent().random
+                self.session = requests.Session()
+                request_retry = request.replace(dont_filter=True)
+                return request_retry
             # 创建 Scrapy 的 HtmlResponse 对象
             # 如果请求失败，重新生成请求
-            if 'seccaptcha.haplat.net/css/captcha.css' in response.text or response.status_code != 200:
-                print("重试")
+            if ('seccaptcha.haplat.net/css/captcha.css' or '访问行为存在异常') in response.text:
+                # print(response.request.url)
+                # print(response.request.body)
+                # print(response.text)
+                # print("重试")
+                self.proxy = self.pool.load_proxy(from_clash=True)
+                # meta_ = request.meta.copy()
+                # meta_['proxy'] = self.proxy
+                self.pdf_headers['User-Agent'] = UserAgent().random
+                self.headers['User-Agent'] = UserAgent().random
+                self.session = requests.Session()
+                # request_retry = request.replace(dont_filter=True, headers=self.headers, meta=meta_)
                 request_retry = request.replace(dont_filter=True)
                 return request_retry
             return HtmlResponse(url=request.url, body=response.content, encoding='utf-8', request=request)
